e7634c642625df645c8226cc2dbd90b2d15471ca
jauregi13 <jon.j1305@gmail.com>
2023-12-07 21:42:08 +0100

    tests de los diferentes botones hechos

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,52 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ButtonStyled from './components/ButtonStyled';
-import 'jest-styled-components';
-import 'jest-dom/extend-expect';
+import { ButtonStyled } from './components/ButtonStyled';
+import './setupTests'
+
+
 
 describe("TEST BUTTON STYLE",() => {
 
-    test("Renderizar botón ", () => {
+    test("Renderizar botón cualquiera", () => {
     
         render(<ButtonStyled data-testid="button-active">ACTIVE</ButtonStyled>)
         const button = screen.getByTestId('button-active')
         expect(button).toBeInTheDocument()
 
     })
+
+    test("Comprobar que el botón Active tiene el color del texto adecuado", () => {
+
+        render(<ButtonStyled data-testid="button-active" status="active">Active</ButtonStyled>)
+        const button = screen.getByTestId('button-active')
+        expect(button).toHaveStyle('color: #5AD07A')
+
+    })
+
+    test("Comprobar que el botón Canceled tiene el color del fondo adecuado", () => {
+
+        render(<ButtonStyled data-testid="button-canceled" status="canceled">Canceled</ButtonStyled>)
+        const button = screen.getByTestId('button-canceled')
+        expect(button).toHaveStyle('background-color: #575757')
+
+    })
+
+    test("Comprobar que el botón Available tiene el color del fondo adecuado", () => {
+
+        render(<ButtonStyled data-testid="button-available" status="Available">Available</ButtonStyled>)
+        const button = screen.getByTestId('button-available')
+        expect(button).toHaveStyle('background-color: #5AD07A')
+
+    })
+
+    test("Comprobar que el botón View Notes de Special Request tiene el color del fondo adecuado", () => {
+
+        render(<ButtonStyled data-testid="button-viewNotes" request="true">View Notes</ButtonStyled>)
+        const button = screen.getByTestId('button-viewNotes')
+        expect(button).toHaveStyle('background-color: #EEF9F2')
+
+    })
+
+
 })
